ba88a6568cc0044bca54d7debb90aa65098997f7
evilkeil <114153280+evilkeil@users.noreply.github.com>
2023-12-05 17:16:55 +0900

    check box

--- app.js
+++ app.js
@@ -1,60 +1,99 @@
-function pageTransition(){
-    let tl = gsap.timeline();
-    return tl.to('.transition',{
-        duration:1,
-        scaleY:1,
-        trasnformOrigin:'bottom',
-        ease:"power4.inOut",
-    })
+// function pageTransition(){
+//     let tl = gsap.timeline();
+//     return tl.to('.transition',{
+//         duration:1,
+//         scaleY:1,
+//         trasnformOrigin:'bottom',
+//         ease:"power4.inOut",
+//     })
 
+// }
+
+// function temp(){
+//     let tl = gsap.timeline();
+//   return  tl.to('.transition',{
+//         duration:1,
+//         scaleY:0,
+//         trasnformOrigin:'top',
+//         ease:"power4.inOut",
+//         delay:0.2,
+//     })
+
+// }
+
+
+// function contentAnimation(){
+//     tl.to('h1',{
+//         duration:1,
+//         top:0,
+//         ease:"power4.inOut",
+//         delay:0.75,
+//     })
+// }
+
+// function delay(n){
+//     n = n||0;
+//     return new Promise((done)=>{
+//         setTimeout(()=>{
+//             done();
+//         },n);
+//     });
+// }
+
+// barba.init({
+//     sync:true,
+//     transitions:[
+//         {
+//             async leave(data){
+//                 const done = this.async();
+//                 pageTransition();
+//                 // temp();
+//                 await delay(1000);
+//                 done();
+//             },
+//             async enter(data){
+//                 temp();
+//                 //  contentAnimation();
+//             }
+//         }
+//     ]
+// })
+
+
+const checkboxes = document.querySelectorAll('.option');
+const span = document.getElementById('result');
+let selected = 0;
+
+
+function addSelection(){
+    selected++
 }
 
-function temp(){
-    let tl = gsap.timeline();
-  return  tl.to('.transition',{
-        duration:1,
-        scaleY:0,
-        trasnformOrigin:'top',
-        ease:"power4.inOut",
-        delay:0.2,
-    })
 
+function removeSelection(){
+    selected--
 }
 
-
-function contentAnimation(){
-    tl.to('h1',{
-        duration:1,
-        top:0,
-        ease:"power4.inOut",
-        delay:0.75,
-    })
+function clearText(){
+    span.textContent = "";
+}
+function updateText(){
+    span.textContent =`${selected}`;
 }
 
-function delay(n){
-    n = n||0;
-    return new Promise((done)=>{
-        setTimeout(()=>{
-            done();
-        },n);
-    });
-}
-
-barba.init({
-    sync:true,
-    transitions:[
-        {
-            async leave(data){
-                const done = this.async();
-                pageTransition();
-                // temp();
-                await delay(1000);
-                done();
-            },
-            async enter(data){
-                temp();
-                //  contentAnimation();
-            }
+checkboxes.forEach(checkbox => {
+    checkbox.addEventListener('change',(e)=>{
+        if(checkbox.checked){
+            addSelection();
+            clearText()
+            updateText()
+            console.log(selected)
         }
-    ]
+        else{
+            removeSelection()
+            clearText()
+            updateText()
+            console.log(selected)
+        }
+    })
 })
