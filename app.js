8a8804eb2bc6db62a9e9bc75169818535f4a957e
evilkeil <114153280+evilkeil@users.noreply.github.com>
2023-12-07 18:15:00 +0900

    write code for text change

--- app.js
+++ app.js
@@ -61,8 +61,19 @@
 
 
 const checkboxes = document.querySelectorAll('.option');
-const span = document.getElementById('result');
+const span = document.getElementById('number');
+const h2 = document.getElementById('result');
 let selected = 0;
+
+
+const resultList = {
+  0:"現在薄毛の可能性は低いですが、症状が出る前に予防治療を行うこともできます。",
+  1:"女性の薄毛のサインが出ているので、まずは当院で実施している無料診断をおすすめします",
+  2:"女性の薄毛である可能性が高いので、毛根が死滅する前に薄毛治療を行うことが大切です。"
+}
+
+console.log(resultList[1])
+
 
 
 function addSelection(){
@@ -76,24 +87,71 @@ function removeSelection(){
 
 function clearText(){
     span.textContent = "";
+    result.textContent=""
+
 }
-function updateText(){
+function updateText(n){
     span.textContent =`${selected}`;
+    h2.textContent =`${resultList[n]}`;
+    if(n === 1){
+      h2.style.color = 'blue';
+    }else if(n === 2){
+      h2.style.color = 'red';
+    }
+}
+
+function determineResult(n){
+  
+if(n >= 1 && n < 3){
+  return 1;
+}else if(n >= 3){
+  return 2;
+}else{
+  return 0
+}
 }
 
 checkboxes.forEach(checkbox => {
     checkbox.addEventListener('change',(e)=>{
         if(checkbox.checked){
+       
             addSelection();
+            const result = determineResult(selected);
             clearText()
-            updateText()
+            updateText(result)
             console.log(selected)
         }
         else{
             removeSelection()
+            const result = determineResult(selected);
             clearText()
-            updateText()
+            updateText(result)
             console.log(selected)
         }
     })
 })
+
+
+
+
+
+
+
+
+
+
+// const button = document.querySelector(".button-one");
+
+
+//   button.addEventListener("click", () => {
+//     const currentState = button.getAttribute("data-state");
+
+//     if (!currentState || currentState === "closed") {
+//       button.setAttribute("data-state", "opened");
+//       button.setAttribute("aria-expanded", "true");
+//     } else {
+//       button.setAttribute("data-state", "closed");
+//       button.setAttribute("aria-expanded", "false");
+//     }
+//   });
+
